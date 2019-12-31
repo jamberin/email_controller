a52b6ffe83a44b9af5abc547ca2cc9af9fd1b6f5
James Beringer <jamberin@gmail.com>
2019-12-30 22:41:02 -0500

    [INIT] Initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r') as read_me:
+    description = read_me.read()
+
+setuptools.setup(
+    name='email_controller',
+    version='0.0.1',
+    author='James Beringer',
+    author_email='jamberin@gmail.com',
+    description='Package for just basic SMTP email sending',
+    long_description=description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/jamberin/email_controller',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: Public Domain'
+    ],
+    include_package_data=True
+)
