162a0138c34785fb65447c4a1f4b0d6e1ad8d23d
James Beringer <jamberin@gmail.com>
2020-01-26 21:46:03 -0500

    [0.0.4] Hotfix for adjusting to static files

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as read_me:
 
 setuptools.setup(
     name='email_controller',
-    version='0.0.3',
+    version='0.0.4',
     author='James Beringer',
     author_email='jamberin@gmail.com',
     description='Package for just basic SMTP email sending',
